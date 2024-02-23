00f6fb5a3a19ae2db6a011f60bff07806c4a6dd5
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-02-23 18:48:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,8 +113,8 @@ def main():
         st.write("- Checkboxes for Follow-up Instructions")
 
     # Notes and Comments Section
-    Notes_and_Comments = st.text_input('Notes and Comments')
-    st.write(st.markdown('<h4>Notes and Comments</h4>', unsafe_allow_html=True))
+    Notes_and_Comments = st.text_input(label=st.markdown('<h4>Notes and Comments</h4>', unsafe_allow_html=True))
+
 
     # Submit Button
     if st.button('Submit'):
