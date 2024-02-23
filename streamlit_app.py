644310bcbfcbd0c1d5422d710b5ffc8f44bb51dd
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-02-23 21:17:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -274,8 +274,12 @@ def past_patient_reports_page():
                 for record in records:
                     # Filter out None values
                     filtered_record = {key: record[i] for i, key in enumerate(cursor.column_names) if record[i] is not None}
-                    # Display the filtered record in a colored box
-                    st.markdown('<div class="box">' + str(filtered_record) + '</div>', unsafe_allow_html=True)
+                    # Display the filtered record in the desired format
+                    st.markdown('<div class="box">', unsafe_allow_html=True)
+                    for key, value in filtered_record.items():
+                        st.write(f"{key}: {value}")
+                    st.markdown('</div>', unsafe_allow_html=True)
+
             else:
                 st.write(f"No patient records found for {search_type}: {search_value}")
         except ValueError as ve:
