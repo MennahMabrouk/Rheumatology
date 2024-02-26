553efc5c9415d04907966692b5eb3ce690a82f4f
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-02-26 19:30:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,13 +68,6 @@ def new_patient_page():
     age = st.number_input('Age', min_value=0, max_value=150, value=0, step=1)
     gender = st.selectbox('Gender', ['Male', 'Female'])
 
-    try:
-        # Insert patient information into the Patient table
-        cursor.execute("INSERT INTO Patient (name, age, gender) VALUES (%s, %s, %s)", (name, age, gender))
-        conn.commit()
-        # Get the auto-generated patient_id
-        patient_id = cursor.lastrowid
-        
         # Medical History Section
         st.markdown('<div class="box"><h4>Medical History</h4></div>', unsafe_allow_html=True)
 
